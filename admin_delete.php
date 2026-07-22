9abdded76e1d6a180cc37eb73bc9cd0a448ca97a
Tharuka <tharukahansi83@gmail.com>
2026-07-22 23:08:11 +0530

    soloved delete page issue

--- admin_delete.php
+++ admin_delete.php
@@ -8,17 +8,27 @@ if($_SESSION['role'] != "admin"){
 }
 
 $type = $_GET['type'];
-$id   = $_GET['id'];
+$id   = (int)$_GET['id'];
+
+mysqli_query($conn, "SET FOREIGN_KEY_CHECKS = 0");
 
 if($type == "user"){
-    mysqli_query($conn,"DELETE FROM users WHERE user_id=$id");
+
+    mysqli_query($conn, "DELETE FROM customer WHERE user_id=$id");
+    mysqli_query($conn, "DELETE FROM vendor WHERE user_id=$id");
+    mysqli_query($conn, "DELETE FROM event WHERE user_id=$id");
+    
+    mysqli_query($conn, "DELETE FROM users WHERE user_id=$id");
 }
 elseif($type == "vendor"){
-    mysqli_query($conn,"DELETE FROM vendor WHERE vendor_id=$id");
+    mysqli_query($conn, "DELETE FROM vendor WHERE vendor_id=$id");
 }
 elseif($type == "product"){
-    mysqli_query($conn,"DELETE FROM product WHERE product_id=$id");
+    mysqli_query($conn, "DELETE FROM product WHERE product_id=$id");
 }
 
+mysqli_query($conn, "SET FOREIGN_KEY_CHECKS = 1");
+
 header("Location: admin.php");
+exit();
 ?>
